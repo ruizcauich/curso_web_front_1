9e09cfd861c2260906a146d18a66adacf81a72d4
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2020-10-12 14:02:37 -0500

    Agrega segunda parte quueriSelector y data-

--- 4.JavaScript_Intermedio&DOM/5.0.0.0.querySelector/README.md
+++ 4.JavaScript_Intermedio&DOM/5.0.0.0.querySelector/README.md
@@ -1,4 +1,4 @@
-# Localizando elementos del DOM usando selectores CSS
+# Localizando elementos del DOM usando selectores CSS y data-
 
 Hasta este momento hemos empleado únicamente el método **getElementById** de **document** para localizar los elementos del DOM.
 
@@ -14,7 +14,14 @@ Ejemplo:
 
 **querySelector** únicamente retornará el primer elemento que coincida con los selectores; para obtener todos los elementos coincidentes, emplea **querySelectorAll**.
 
+Por otra parte aprenderemos a definir propiedades personalizadas que nos pueden servir
+de mucha ayuda al trabajar con JavaScript y crear código que funcione para más de un
+elemento del mismo tipo: Modales, Notificaciones, etc.
+
+Lo anterior definiendo data-atributo en HTML y empleandolo desde JavaScript.
+
 Enlaces:
 
 1. [https://developer.mozilla.org/es/docs/Web/API/Document/querySelector](https://developer.mozilla.org/es/docs/Web/API/Document/querySelector)
 2. [https://developer.mozilla.org/es/docs/Web/API/Document/querySelectorAll](https://developer.mozilla.org/es/docs/Web/API/Document/querySelectorAll)
+3. [https://developer.mozilla.org/es/docs/Learn/HTML/como/Usando_atributos_de_datos](https://developer.mozilla.org/es/docs/Learn/HTML/como/Usando_atributos_de_datos)
--- 4.JavaScript_Intermedio&DOM/5.0.0.0.querySelector/script.js
+++ 4.JavaScript_Intermedio&DOM/5.0.0.0.querySelector/script.js
@@ -8,10 +8,6 @@ let deleteMessageButtons = document.querySelectorAll(".message .delete");
 // NOTIFICACIONES
 let deleteNotificationButtons = document.querySelectorAll(".notification .delete");
 
-// MODALES
-let botonOpenModal = document.getElementById("openModal")
-let modal = document.getElementById("miModal");
-let closeModalButton = document.querySelector(".modal-close")
 
 
 // deleteMessageButton.addEventListener("click", function(){
@@ -30,10 +26,46 @@ deleteNotificationButtons.forEach(function(button){
     })
 });
 
-botonOpenModal.addEventListener("click", function(){
-    modal.classList.add("is-active");
+
+// MODALES
+// El codigo siguiente tiene una limitante, se maneja por medio de ids
+// let botonOpenModal = document.getElementById("openModal")
+// let modal = document.getElementById("miModal");
+let closeModalButtons = document.querySelectorAll(".modal-close")
+
+// Si queremos modularizar y usar el mismó código
+// debemos saber cómo identificar al modal que quiere
+// se debe abrir al dar click con algún botón, lo mismo para identificar
+// cada botón que tiene como objetivo abrir un modal
+
+// Los atributos personalizados (data-atributo), permiten establecer
+// valores que podemos usar para para diferentes fines: identificar acciones,
+// identificar el objetivo de una acción, etc.
+// elejimos data-toggle para saber que el boton debe hacer un toggle
+// y cmo valor modal para saber que esta acción se debe llevar a cabo en
+// un modal
+let openModalButtons = document.querySelectorAll("[data-toggle=modal]");
+
+openModalButtons.forEach(function(button){
+    button.addEventListener("click", function(){
+        // Debemos saber sobre qué modal, usemos de nuevo
+        // los atributos personalizados, tenemos acceso a estos
+        // por medio del objeto .dataset
+        let target = button.dataset["target"];
+        let modal = document.querySelector(target);
+        modal.classList.add("is-active")
+    })
 })
 
-closeModalButton.addEventListener("click", function(){
-    modal.classList.remove("is-active");
-})
+// botonOpenModal.addEventListener("click", function(){
+//     modal.classList.add("is-active");
+// })
+
+closeModalButtons.forEach(function(button){
+    button.addEventListener("click", function(){
+        console.log(button.parentElement.classList)
+        button.parentElement.classList.remove("is-active");
+    });
+});
+
+
